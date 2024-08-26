b6f2473c07fdae2495e26e6da4b64833cab55d5c
Shafi2016 <56795978+Shafi2016@users.noreply.github.com>
2024-08-26 10:04:57 -0600

    Update news_updated.py

--- news_updated.py
+++ news_updated.py
@@ -44,7 +44,7 @@ with st.sidebar:
 st.markdown("""
     <div style="background-color:#0077a8;padding:10px;border-radius:5px;width:100%;max-width:1200px;margin: 0 auto;">
         <h1 style="color:white;">SpotLight News</h1>
-        <p style="color:white; font-size:18px; white-space: nowrap; text-align:center;">Spotlight the stories that matter with quick summaries and curated insights, delivered instantly.</p>
+        <p style="color:white; font-size:17px; white-space: nowrap; text-align:center;">Spotlight the stories that matter with quick summaries and curated insights, delivered instantly.</p>
     </div>
 """, unsafe_allow_html=True)
 
